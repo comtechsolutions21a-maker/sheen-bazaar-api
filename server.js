2bd38ade85ba44d0db09ca4d4ded151058f0cd54
comtechsolutions21a-maker <comtechsolutions21a@gmail.com>
2026-07-29 11:06:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -57,5 +57,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`Sheen Bazaar API running on port ${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => console.log(`Sheen Bazaar API running on port ${PORT}`));
 });
